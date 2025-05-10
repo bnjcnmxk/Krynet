84adab0caa5b6742d18282d0a4dffbbd56988973
4zx <78288792+4zx16@users.noreply.github.com>
2025-05-10 16:24:19 -0400

    Update src.js
    Signed-off-by: 4zx <78288792+4zx16@users.noreply.github.com>

--- src.js
+++ src.js
@@ -1,1 +1,36 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const helmet = require('helmet');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('message', msg => {
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Krynet API Live');
+});
+
+server.listen(3001, () => console.log('Server running on port 3001'));
